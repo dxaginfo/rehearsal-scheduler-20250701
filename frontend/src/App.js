21e9c9b32c39a894f648ac7027cd8ed4f1dfcc16
dxaginfo <142343166+dxaginfo@users.noreply.github.com>
2025-07-01 05:18:32 -0300

    Initial React app boilerplate for rehearsal scheduler front-end

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <h1>Rehearsal Scheduler</h1>
+      <p>Web app for band rehearsal scheduling and attendance tracking.</p>
+      {/* Navigation for Calendar, Setlists, Chat, etc. */}
+    </div>
+  );
+}
+export default App;
